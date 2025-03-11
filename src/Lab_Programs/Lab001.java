43138d5f7132b498a4e6d9c5485bbd39041ca6f4
Pandiyarajan M <pandiyarajanmk@gmail.com>
2025-03-11 22:35:09 +0530

    My Second Push

--- src/Lab_Programs/Lab001.java
+++ src/Lab_Programs/Lab001.java
@@ -2,6 +2,7 @@ package Lab_Programs;
 
 public class Lab001 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Rajan");
+
     }
 }
---
+++ src/Lab_Programs/Lab002.java
@@ -0,0 +1,20 @@
+package Lab_Programs;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int a = 9;
+        int num = 1;
+        int result;
+
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+        System.out.printf("%d x %d = %d  \n",a,num,result=a*num++);
+    }
+}
