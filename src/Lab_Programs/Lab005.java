03b20f9c1f3091ba719ab0b496c3cc6d5deb11eb
Pandiyarajan M <pandiyarajanmk@gmail.com>
2025-03-13 07:50:33 +0530

    My Five Push

--- src/Lab_Programs/Lab005.java
+++ src/Lab_Programs/Lab005.java
@@ -1,4 +1,12 @@
 package Lab_Programs;
 
 public class Lab005 {
+    public static void main(String[] args) {
+        byte age = 2;
+
+        String Vote = age>= 18 ? "You can Vote" : "You can't Vote";
+
+        System.out.println(Vote);
+
+    }
 }
