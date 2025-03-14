2e78a84641b939ba772fc36fb917acc8a1ffbd11
Pandiyarajan M <pandiyarajanmk@gmail.com>
2025-03-14 07:50:07 +0530

    My Five Push

---
+++ src/Lab006.java
@@ -0,0 +1,13 @@
+public class Lab006 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+
+        // ++a - > A -> ExpA = 11 -> a=11
+        // a++ -> B -> ExpB = 11 -> a=12
+        // a++ -> C -> ExpC = 12 -> a=13
+
+        //ExpA + ExpB+ ExpC = 11+11+12 = 34
+
+    }
+}
